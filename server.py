509e8a8517413cfd0c15cd7f7f6bc509dfb3f7a5
Kai Peng <kai.peng.wh13@wharton.upenn.edu>
2015-03-07 17:28:14 -0600

    separated DataManager. added settings. ouput stops to_json to be orient='records'

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import tornado.ioloop
 import tornado.web
 from tornado.options import define, options, parse_command_line
 
-from data_loading import load_stops
+from data_loading import load_stops, load_calendar, load_stop_times, load_trips
 
 from fetch_cta_data import fetch_cta_data
 from get_nearest_stop import get_nearest_stop
@@ -12,6 +12,13 @@ NUMBER = r'-?[0-9]+\.*[0-9]*'
 
 define("port", default=8888, help="run on the given port", type=int)
 
+class DataManager():
+    def __init__(self):
+        fetch_cta_data()
+        self.stops = load_stops()
+        self.calendar = load_calendar()
+        self.stop_times = load_stop_times()
+        self.trips = load_trips()
 
 class DefaultHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
@@ -25,20 +32,24 @@ class NearestStopHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self, my_lat, my_lon):
         self.set_header("Content-Type", "text/json")
-        stops = load_stops()
+        stops = data_manager.stops
         stop = get_nearest_stop(stops, [float(my_lat), float(my_lon)])
-        self.write(stop.to_json())
+        self.write(stop.to_json(orient='records'))
         self.finish()
 
-
+settings = {'debug': True}
 application = tornado.web.Application([
     (r'/', DefaultHandler),
     ((r'/stops/(%s)/(%s)' % (NUMBER, NUMBER)), NearestStopHandler),
-])
+], **settings)
+
+data_manager = None
 
 if __name__ == '__main__':
-    fetch_cta_data()
-
     parse_command_line()
+    print "Loading Data"
+    data_manager = DataManager()
+    print "Finished Loading"
+
     application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
