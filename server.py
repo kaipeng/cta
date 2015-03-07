c1a62de24d008d8e53c2a5e11d1d356b5bce95d4
Will Slotkin <william.slotkin@gmail.com>
2015-03-07 13:34:16 -0600

    create server skeleton

--- server.py
+++ server.py
@@ -1,2 +1,23 @@
-import tornado
+import tornado.ioloop
+import tornado.web
+from tornado.options import define, options, parse_command_line
 
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class DefaultHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("Success!")
+        self.finish()
+
+
+application = tornado.web.Application([
+    (r'/', DefaultHandler),
+])
+
+if __name__ == '__main__':
+    parse_command_line()
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
